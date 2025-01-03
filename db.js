63f6986ae663f25eb2cf9d70a1ceb5ae0de5f27b
BIKRAMSINGH1221 <bikomaster9459@gmail.com>
2025-01-03 16:53:42 +0530

    Change the db.js again

--- db.js
+++ db.js
@@ -1,30 +1,26 @@
 const { Pool } = require('pg');
-const { config } = require('dotenv');
-
-// Load environment variables
+const {config} = require('dotenv');
 config();
-
 // Set up PostgreSQL connection
-const poolConfig = process.env.DB_URL
-  ? { connectionString: process.env.DB_URL, ssl: { rejectUnauthorized: false } }
-  : {
-      user: process.env.DB_USER,
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT,
-      ssl: { rejectUnauthorized: false }, // Enable/disable as needed
-    };
+const pool = new Pool({
+    connectionString: process.env.DB_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // Set to true if you want to reject self-signed certificates
+  }                  // Default PostgreSQL port
+});
 
-const pool = new Pool(poolConfig);
-
-// Test the connection
+// Test connection
 pool.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-  } else {
-    console.log('Connected to the PostgreSQL database');
-  }
+    if (err) {
+        console.error('Error connecting to the database:', err);
+    } else {
+        console.log('Connected to the PostgreSQL database');
+    }
 });
 
 module.exports = pool;
