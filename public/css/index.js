77730026ddd7bcdb1e2b6a78f8da5c54190baf1c
BIKRAMSINGH1221 <bikomaster9459@gmail.com>
2025-01-07 08:41:10 +0530

    GSAP added

---
+++ public/css/index.js
@@ -0,0 +1,17 @@
+gsap.from(".div2 h1",{
+    x:-800,
+    duration : 1.1,
+    opacity : 0
+})
+
+gsap.from(".div2 p",{
+    x:800,
+    duration : 1.3,
+    opacity : 0
+})
+
+gsap.from(".div2 .allButtons",{
+    y:800,
+    duration : 1.8,
+    opacity :-4
+})
